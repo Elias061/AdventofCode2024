47b188c39368bce50f520a4402941f928d4fb3af
FileShareAirbus <filetransgit@gmail.com>
2024-12-13 13:58:11 +0100

    sgh
    gfhdfh

---
+++ Day2/Day2Task2.py
@@ -0,0 +1,37 @@
+number=[]
+
+def Read_File():
+    
+    file = open("C:\\Users\\beel174\\Documents\\GitHub\\AdventofCode2024\\Day2\\source.txt")
+    content = file.read()
+    content = content.split("\n")
+    return content
+
+
+def data_handling(content):
+    for index in content:
+        einzelne_zahlen=index.split(" ")
+        number.append(einzelne_zahlen)
+    return number
+    
+
+def mode_increasing(number):
+    index=0
+    validList= []
+    for liste in number:
+        index = 0
+        StatusValide = True
+        for element in liste:
+            if int(element) > int(liste[index]):
+                print(f"valid {element}")
+                #StatusValide= True
+            else:
+                print(f"huso {element}")    
+                StatusValide = False
+        if StatusValide:
+            validList.append(liste)
+           
+
+content = Read_File()
+data_handling(content)
+mode_increasing(number)
